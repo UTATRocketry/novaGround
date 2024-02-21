a740c5080c51e38e2d56b4a62809248695ecb783
Daniel G. Pinto <danielpintolzr@gmail.com>
2024-02-21 17:24:49 -0500

    Fixed actuators.cpp

--- src/actuators/actuators.cpp
+++ src/actuators/actuators.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 bool change_state(bool current_state) {
-    std::cout << "State changed";
-    return not(current_state);
+    bool changed_state = not(current_state);
+    std::cout << "State changed to " << changed_state;
+    return changed_state;
 }
