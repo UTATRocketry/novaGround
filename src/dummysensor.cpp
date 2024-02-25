c64ec58a8bb380eeed3904fe188d621ae26416ac
utatRocketryPi <utatRocketryPi@gmail.com>
2024-02-25 15:45:24 -0500

    dummysensor.cpp now produces random numbers - good

--- src/dummysensor.cpp
+++ src/dummysensor.cpp
@@ -4,7 +4,17 @@
 #include "dummysensor.h"
 
 double dummysensor(double lower, double upper) {
+    std::random_device rd;
+    std::default_random_engine re(rd());
     std::uniform_real_distribution<double> unif(lower, upper);
-    std:: default_random_engine re;
     return unif(re);
 }
+
+// for testing
+//int main(){
+//	for (int i = 0; i<10; i++){
+//        double my_num = dummysensor(3.0, 4.0);
+//        std::cout<<my_num<<std::endl;
+//	}
+//	return 0;
+//}
