68e9b26d4c7fda320a3407c808f9e4c1f8d21067
Sebastien Psarianos <sebastienpsarianos@Sebastiens-MacBook-Pro.local>
2024-04-04 16:20:07 -0400

    setting up clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "entities/dataAcquisition/Sensor/DummySensor.h"
 
 auto main() -> int{
-	std::cout << "TEST123" << "\n";
+	std::cout << "TEST12356" << "\n";
 	auto *test = new DummySensor();
 
 	std::cout << test->readData() << "\n";
