f1eade3d885bb17d85010ccf0193c59f82ab19e4
Sebastien Psarianos <sebastien@psarianos.com>
2024-05-10 18:14:57 -0400

    Waited way too long before commiting -_-

--- src/interfaces/Actuators/Actuator/DummyActuator.h
+++
@@ -1,9 +0,0 @@
-#include "Actuator.h"
-
-class DummyActuator : private Actuator<bool> {
-  public:
-    explicit DummyActuator(int id) : Actuator(id){};
-    auto setActuatorState(bool state) -> int override;
-
-    int id;
-};
