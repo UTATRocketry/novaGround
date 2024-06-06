a19b9ac6075f98da7c2caa05a9fd9c360bb8a35f
Sebastien Psarianos <50034807+SebastienPsarianos@users.noreply.github.com>
2024-06-06 15:50:18 -0700

    Ft 67 class structure (#8)
    * Class structure set up first draft. Meson working with linting and format (#4)
    
    * first draft of class structure
    
    * Couple of useful functions in SensorTypeManager
    
    * moved entities into folders
    
    * set up dummySensor class
    
    * cleanup
    
    * implemented dummy read data
    
    * meson build config stuff
    
    * Added google basic clang-tidy config, started on readme file
    
    * more readme
    
    * more readme
    
    * clang-tidy stuff
    
    * still working on clang-tidy
    
    * fixing clang-tidy
    
    * clang-tidy linting stuff set up
    
    * setting up clang-format
    
    * mroe clang format stuff
    
    * started on actuators
    
    * most classes done
    
    * class structure first draft
    
    ---------
    
    Co-authored-by: Sebastien Psarianos <sebastienpsarianos@Sebastiens-MacBook-Pro.local>
    
    * Tk 67 6 class structure (#7)
    
    * first draft of class structure
    
    * Couple of useful functions in SensorTypeManager
    
    * moved entities into folders
    
    * set up dummySensor class
    
    * cleanup
    
    * implemented dummy read data
    
    * meson build config stuff
    
    * Added google basic clang-tidy config, started on readme file
    
    * more readme
    
    * more readme
    
    * clang-tidy stuff
    
    * still working on clang-tidy
    
    * fixing clang-tidy
    
    * clang-tidy linting stuff set up
    
    * setting up clang-format
    
    * mroe clang format stuff
    
    * started on actuators
    
    * most classes done
    
    * class structure first draft
    
    * removed line causing build error
    
    * changed entities to interfaces
    
    * Waited way too long before commiting -_-
    
    * more stuff
    
    ---------
    
    Co-authored-by: Sebastien Psarianos <sebastienpsarianos@Sebastiens-MacBook-Pro.local>
    
    ---------
    
    Co-authored-by: Sebastien Psarianos <sebastienpsarianos@Sebastiens-MacBook-Pro.local>

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,49 @@
-#include "entities/control/Actuator/DummyActuator.h"
-#include "entities/dataAcquisition/Sensor/DummySensor.h"
+#include "interfaces/ActuatorInterface/ActuatorManager/DummyActuatorManager.h"
+#include "interfaces/SensorInterface/SensorManager/DummySensorManager.h"
+
 #include <iostream>
+#include <string>
+#include <tuple>
+#include <vector>
 
 auto main() -> int {
-    std::cout << "TEST12356"
-              << "\n";
+    std::tuple<int, std::string, bool> const actuator_one{0, "Actuator One",
+                                                          true};
+    std::tuple<int, std::string, bool> const actuator_two{1, "Actuator Two",
+                                                          false};
+    std::tuple<int, std::string, bool> const actuator_three{2, "Actuator Three",
+                                                            true};
 
-    auto* sensor = new DummySensor(1);
-    auto* actuator = new DummyActuator(2);
+    std::vector<std::tuple<int, std::string, bool>> actuatorParams{
+        actuator_one, actuator_two, actuator_three};
 
-    int i{0};
+    auto dummyActuator = DummyActuatorManager(actuatorParams);
 
-    while (i++ < 100) {
-        actuator->setActuatorState(i % 2 == 0);
-        std::cout << sensor->readData() << "\n";
+    std::vector<std::tuple<int, std::string, double, double>> sensorParams{
+        {0, "Sensor One", 0, 10},
+        {1, "Sensor Two", 10, 20},
+        {2, "Sensor Three", 20, 30},
     };
 
-    delete sensor;
-    delete actuator;
-}
+    DummySensorManager dummySensorMan = DummySensorManager(sensorParams);
+    std::cout << std::get<0>(dummySensorMan.querySensors()[0]);
+
+    int i{0};
+    while (i++ < 1000) {
+
+        for (std::tuple<int, std::string, bool>& actuator_state :
+             dummyActuator.getActuatorSummary()) {
+            std::cout << "ID: " << std::get<0>(actuator_state) << '\n';
+            std::cout << "Name: " << std::get<1>(actuator_state) << '\n';
+            std::cout << "State: " << std::get<2>(actuator_state);
+            std::cout << '\n' << '\n';
+        }
+
+        for (std::tuple<int, double>& sensorState :
+             dummySensorMan.querySensors()) {
+            std::cout << "ID:" << std::get<0>(sensorState) << "\n";
+            std::cout << "Sensor Value" << std::get<1>(sensorState) << "\n";
+            std::cout << "\n"
+                      << "\n";
+        }
+    }
