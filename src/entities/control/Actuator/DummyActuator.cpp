597c78e7c0bd5c3eed0059a780b436e6178fda2b
Sebastien Psarianos <sebastien@psarianos.com>
2024-06-06 15:58:19 -0700

    fixed merge issues

--- src/entities/control/Actuator/DummyActuator.cpp
+++
@@ -1,8 +0,0 @@
-#include "DummyActuator.h"
-#include <iostream>
-
-auto DummyActuator::setActuatorState(bool state) -> int {
-    this->actuator_state_ = state;
-    std::cout << "Actuation Event " << state << "\n";
-    return 0;
-};
--- src/entities/control/ActuatorManager/DummyActuatorManger.h
+++
@@ -1,6 +0,0 @@
-#include "ActuatorManager.h"
-
-class DummyActuatorManager : public ActuatorManager<bool> {
-    public:
-        DummyActuatorManager();
-};
