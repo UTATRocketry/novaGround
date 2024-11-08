8a16e362208387d34c5869a0afc29897358328e1
blobcode <lhelme123@gmail.com>
2024-11-07 20:31:22 -0500

    daq sampling

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,11 @@
 #include <boost/lockfree/spsc_queue.hpp>
 #include <cctype>
 #include <chrono>
+#include <cstdint>
 #include <cstdlib>
 #include <cstring>
+#include <daqhats/daqhats.h>
+#include <daqhats/mcc128.h>
 #include <format>
 #include <iostream>
 #include <memory>
@@ -20,15 +23,57 @@ const std::string CLIENT_ID("novaground");
 struct sensor_datapoint {
     int id;
     double value;
+    double time;
 };
 
 boost::lockfree::spsc_queue<sensor_datapoint> sensor_buffer{8192};
 
-double get_sensor() {
-    std::default_random_engine rnd{std::random_device{}()};
-    std::uniform_real_distribution<double> dist(0, 100);
+std::vector<int> initialize_daqs() {
+    std::vector<int> connected_daqs;
+    int count = hat_list(HAT_ID_ANY, NULL);
 
-    return dist(rnd);
+    if (count > 0) {
+        struct HatInfo* list =
+            (struct HatInfo*)malloc(count * sizeof(struct HatInfo));
+        hat_list(HAT_ID_ANY, list);
+
+        for (int i = 0; i < count; i++) {
+            connected_daqs.push_back(+list[i].address);
+            // std::cout<< "Address " << +list[i].address <<std::endl;
+            // std::cout<< "Version" << list[i].version<<std::endl;
+            // std::cout<< "Product Name" << list[i].product_name<<std::endl;
+            // std::cout<< "ID" << list[i].id<<std::endl;
+        }
+
+        free(list);
+    }
+
+    return connected_daqs;
+}
+
+// int close_daqs(std::vector<int> list_of_daqs) {
+//     // NOTE: THIS IS HARDCODED FOR MCC128s, AND WILL NOT, IN THE CURRENT
+//     // CONFIGURATION, WORK OTHERWISE. Currently doesn't seem to work.
+//     // result_code keeps returning -1
+//     for (int i = 0; i < (int)list_of_daqs.capacity(); i++) {
+//         std::cout << list_of_daqs[i] << std::endl;
+//         int result_code = mcc128_close((uint8_t)list_of_daqs[i]);
+//         std::cout << +result_code << std::endl;
+//         if (result_code != RESULT_SUCCESS) {
+//             std::cout << "ERROR IN close_daqs()" << std::endl;
+//             return 0;
+//         }
+//     }
+//     return 1;
+// }
+
+// returns daq value, address is board address, channel is analog input
+double get_daq_value(int address, int channel) {
+    double value;
+    int result;
+    uint32_t options = OPTS_DEFAULT;
+    result = mcc128_a_in_read(address, channel, options, &value);
+    return value;
 }
 
 // recv
@@ -44,12 +89,13 @@ void consumer_func(mqtt::async_client_ptr cli) {
     }
 }
 
+// send
 void publisher_func(mqtt::async_client_ptr cli) {
     while (true) {
         boost::json::array data;
         sensor_datapoint sd;
         while (sensor_buffer.pop(sd)) {
-            data.push_back({to_string(sd.id), sd.value});
+            data.push_back({to_string(sd.id), sd.value, sd.time});
         }
         boost::json::value payload = {{"data", data}};
         string s_payload = boost::json::serialize(payload);
@@ -58,12 +104,19 @@ void publisher_func(mqtt::async_client_ptr cli) {
     }
 }
 
-void sample_func() {
+// data sampling thread
+void sample_func(vector<int> daq_ids) {
     while (true) {
-        sensor_datapoint sd;
-        sd.id = 0;
-        sd.value = get_sensor();
-        sensor_buffer.push(sd);
+        for (auto id : daq_ids) {
+            sensor_datapoint sd;
+            sd.id = id;
+            const auto p1 = std::chrono::system_clock::now();
+            sd.value = get_daq_value(0, id);
+            sd.time = std::chrono::duration_cast<std::chrono::seconds>(
+                          p1.time_since_epoch())
+                          .count();
+            sensor_buffer.push(sd);
+        }
 
         // sampling frequency
         this_thread::sleep_for(milliseconds(10));
@@ -71,6 +124,8 @@ void sample_func() {
 }
 
 int main(int argc, char* argv[]) {
+    vector<int> daq_ids = initialize_daqs();
+
     string address = "mqtt://localhost:1883";
 
     // Create an MQTT client using a smart pointer to be shared among threads.
@@ -95,7 +150,7 @@ int main(int argc, char* argv[]) {
         cli->subscribe(TOPICS, QOS);
     }
 
-    std::thread sample(sample_func);
+    std::thread sample(sample_func(daq_ids));
     std::thread consumer(consumer_func, cli);
     std::thread publisher(publisher_func, cli);
 
